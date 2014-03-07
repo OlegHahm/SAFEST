b5cf076fc4c2b4c5066aeb7ce6dfca6bf5e6b015
Oleg Hahm <oleg@hobbykeller.org>
2014-03-07 05:19:54 +0000

    automatically ignore nodes

--- cebit_demo/portal/main.c
+++ cebit_demo/portal/main.c
@@ -39,7 +39,7 @@ const shell_command_t shell_commands[] = {
     {"loop", "", rpl_udp_loop},
     {"server", "Starts a UDP server", udpif_shell_server},
     {"send", "Send a UDP datagram", udpif_shell_send},
-    {"ign", "ignore node", rpl_udp_ignore},
+    {"ign", "ignore node", rpl_udp_ignore_cmd},
     {"fw", "fw an event into the net", portal_in},
     {NULL, NULL, NULL}
 };
@@ -47,18 +47,24 @@ const shell_command_t shell_commands[] = {
 
 void fill_nc(void)
 {
-    uint8_t numof = 5;
-    uint16_t neighbors[] = {31, 32, 33, 41, 51};
+    uint8_t numne = 2;
+    uint8_t numig = 4;
+    uint16_t neighbors[] = {23, 31};
+    uint16_t ignore[] = {32, 33, 41, 51};
     ipv6_addr_t r_addr;
     uint16_t l_addr;
 
-    for (int i = 0; i < numof; i++) {
+    for (int i = 0; i < numne; i++) {
         l_addr = HTONS(neighbors[i]);
         udpif_get_ipv6_address(&r_addr, l_addr);
         ndp_neighbor_cache_add(0, &r_addr, &l_addr, 2, 0,
                                NDP_NCE_STATUS_REACHABLE, 
                                NDP_NCE_TYPE_TENTATIVE, 
                                0xffff);
+    }
+    for (int i = 0; i < numig; i++) {
+        printf("Ignoring %u\n", ignore[i]);
+        rpl_udp_ignore(ignore[i]);
     }
 
 }
